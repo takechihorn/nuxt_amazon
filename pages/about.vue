e6dc1ef603dd3a3bdb87c9d97b8b27f855887b23
Kondo Takeru <takechihorn@gmail.com>
2021-01-15 04:22:27 +0900

    nuxt created

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <input v-model="name" />
+    <button @click="onClick">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Takechi",
+    };
+  },
+  methods: {
+    onClick() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,209 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        admin
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main>
+    <div class="a-spacing-large">
+      <div class="container-fluid browsing-history">
+        <div class="row">
+          <!-- Grid system rowの中のカラムの合計が12 xs-smartphone sm-tablet md-PC lg-largePC
+            12より少ない時は左詰 12より多い時は回り込む
+            ex, col-sm-8:smタブレットより大きい時は12のうち8をとり、smタブレットより小さい場合は横並び全部とる。横画面いっぱい-->
+
+          <div class="col-sm-8 col-8">
+            <h1 class="a-size-large a-spacing-none a-text-normal">
+              All products
+            </h1>
+            <div class="a-spacing-large"></div>
+            <!-- Button -->
+            <a href="#" class="a-button-buy-again">Add a new product</a>
+            <a href="#" class="a-button-history margin-right-10"
+              >Add a new category</a
+            >
+            <a href="#" class="a-button-history margin-right-10"
+              >Add a new owner</a
+            >
+          </div>
+        </div>
+      </div>
+      <!-- Listing page -->
+      <div class="a-spacing-large"></div>
+      <div class="container-fluid browsing-history">
+        <div class="row">
+          <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+            <div class="history-box">
+              <!-- Product image -->
+              <a href="#" class="a-link-normal">
+                <img src="#" alt="img-fluid" />
+              </a>
+              <!-- Product title -->
+              <div class="a-spacing-top-base asin-title">
+                <span class="a-text-normal">
+                  <div class="p13n-sc-truncated">Product title</div>
+                </span>
+              </div>
+              <!-- Product Rating -->
+              <div class="a-row">
+                <a href="#">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </a>
+                <span class="a-letter-space"></span>
+                <span class="a-color-tertiary a-size-small asin-reviews"
+                  >(1732)</span
+                >
+              </div>
+              <!-- Product Price -->
+              <div class="a-row">
+                <span class="a-size-base a-color-price">
+                  <span class="p13n-sc-price">$23</span>
+                </span>
+              </div>
+              <!-- Product Buttons -->
+              <div class="a-row">
+                <a href="#" class="a-button-history margin-right-10">Update</a>
+                <a href="#" class="a-button-history margin-right-10">Delete</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+            <div class="history-box">
+              <!-- Product image -->
+              <a href="#" class="a-link-normal">
+                <img src="#" alt="img-fluid" />
+              </a>
+              <!-- Product title -->
+              <div class="a-spacing-top-base asin-title">
+                <span class="a-text-normal">
+                  <div class="p13n-sc-truncated">Product title</div>
+                </span>
+              </div>
+              <!-- Product Rating -->
+              <div class="a-row">
+                <a href="#">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </a>
+                <span class="a-letter-space"></span>
+                <span class="a-color-tertiary a-size-small asin-reviews"
+                  >(1732)</span
+                >
+              </div>
+              <!-- Product Price -->
+              <div class="a-row">
+                <span class="a-size-base a-color-price">
+                  <span class="p13n-sc-price">$23</span>
+                </span>
+              </div>
+              <!-- Product Buttons -->
+              <div class="a-row">
+                <a href="#" class="a-button-history margin-right-10">Update</a>
+                <a href="#" class="a-button-history margin-right-10">Delete</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+            <div class="history-box">
+              <!-- Product image -->
+              <a href="#" class="a-link-normal">
+                <img src="#" alt="img-fluid" />
+              </a>
+              <!-- Product title -->
+              <div class="a-spacing-top-base asin-title">
+                <span class="a-text-normal">
+                  <div class="p13n-sc-truncated">Product title</div>
+                </span>
+              </div>
+              <!-- Product Rating -->
+              <div class="a-row">
+                <a href="#">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </a>
+                <span class="a-letter-space"></span>
+                <span class="a-color-tertiary a-size-small asin-reviews"
+                  >(1732)</span
+                >
+              </div>
+              <!-- Product Price -->
+              <div class="a-row">
+                <span class="a-size-base a-color-price">
+                  <span class="p13n-sc-price">$23</span>
+                </span>
+              </div>
+              <!-- Product Buttons -->
+              <div class="a-row">
+                <a href="#" class="a-button-history margin-right-10">Update</a>
+                <a href="#" class="a-button-history margin-right-10">Delete</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+            <div class="history-box">
+              <!-- Product image -->
+              <a href="#" class="a-link-normal">
+                <img src="#" alt="img-fluid" />
+              </a>
+              <!-- Product title -->
+              <div class="a-spacing-top-base asin-title">
+                <span class="a-text-normal">
+                  <div class="p13n-sc-truncated">Product title</div>
+                </span>
+              </div>
+              <!-- Product Rating -->
+              <div class="a-row">
+                <a href="#">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </a>
+                <span class="a-letter-space"></span>
+                <span class="a-color-tertiary a-size-small asin-reviews"
+                  >(1732)</span
+                >
+              </div>
+              <!-- Product Price -->
+              <div class="a-row">
+                <span class="a-size-base a-color-price">
+                  <span class="p13n-sc-price">$23</span>
+                </span>
+              </div>
+              <!-- Product Buttons -->
+              <div class="a-row">
+                <a href="#" class="a-button-history margin-right-10">Update</a>
+                <a href="#" class="a-button-history margin-right-10">Delete</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
-export default {}
+export default {
+  // axiosとはAPIを呼び出すLibrary
+  //asyncDataとはNuxt固有の関数でローディングが完了する前にデータをフェッチする関数
+  //SEOに良い。何故ならばデータが先に読み込まれるから
+  async asyncData({ $axios }) {
+    try {
+      let response = await $get("http://localhost:3000/api/products");
+      return {
+        products: response.products,
+      };
+    } catch (error) {}
+  },
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
